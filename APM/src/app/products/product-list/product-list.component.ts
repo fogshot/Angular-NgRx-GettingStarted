05752e6f2b06527edb7f01e98b75e08894c60fa0
Peter von Niebelschütz <fogshot@gmail.com>
2023-07-23 12:30:05 +0200

    use state over service in product list component

--- APM/src/app/products/product-list/product-list.component.ts
+++ APM/src/app/products/product-list/product-list.component.ts
@@ -1,65 +1,71 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import { Subscription } from 'rxjs';
+import { combineLatest, Observable } from 'rxjs';
 
 import { Product } from '../product';
-import { ProductService } from '../product.service';
 import { Store } from '@ngrx/store';
+import {
+  getCurrentProduct,
+  getError,
+  getProducts,
+  getShowProductCode,
+  State,
+} from '../state/product.reducer';
+import * as ProductActions from '../state/product.actions';
+import { loadProducts } from '../state/product.actions';
+import { map } from 'rxjs/operators';
+
+interface VM {
+  products: Product[];
+  selectedProduct: Product;
+  displayCode: boolean;
+}
 
 @Component({
   selector: 'pm-product-list',
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.css'],
 })
-export class ProductListComponent implements OnInit, OnDestroy {
+export class ProductListComponent implements OnInit {
   pageTitle = 'Products';
-  errorMessage: string;
+  products$: Observable<Product[]>;
+  selectedProduct$: Observable<Product>;
+  displayCode$: Observable<boolean>;
+  vm$: Observable<VM>;
+  private errorMessage$: Observable<string>;
 
-  displayCode: boolean;
-
-  products: Product[];
-
-  // Used to highlight the selected product in the list
-  selectedProduct: Product | null;
-  private _sub: Subscription;
-
-  constructor(
-    private productService: ProductService,
-    private store: Store<any>,
-  ) {}
+  constructor(private store: Store<State>) {}
 
   ngOnInit(): void {
-    this._sub = this.productService.selectedProductChanges$.subscribe(
-      (currentProduct) => (this.selectedProduct = currentProduct),
+    this.store.dispatch(loadProducts());
+    this.products$ = this.store.select(getProducts);
+    this.selectedProduct$ = this.store.select(getCurrentProduct);
+    this.displayCode$ = this.store.select(getShowProductCode);
+    this.errorMessage$ = this.store.select(getError);
+    this.vm$ = combineLatest([
+      this.products$,
+      this.selectedProduct$,
+      this.displayCode$,
+    ]).pipe(
+      map(
+        ([products, selectedProduct, displayCode]): VM => ({
+          products,
+          selectedProduct,
+          displayCode,
+        }),
+      ),
     );
-
-    this.productService.getProducts().subscribe({
-      next: (products: Product[]) => (this.products = products),
-      error: (err) => (this.errorMessage = err),
-    });
-
-    this._sub.add(
-      this.store.select('products').subscribe((products) => {
-        if (products) {
-          this.displayCode = products.productReducer.showProductCode;
-        }
-      }),
-    );
-  }
-
-  ngOnDestroy(): void {
-    this._sub.unsubscribe();
   }
 
   checkChanged(): void {
-    this.store.dispatch({ type: '[Product] Toggle Product Code' });
+    this.store.dispatch(ProductActions.toggleProductCode());
   }
 
   newProduct(): void {
-    this.productService.changeSelectedProduct(this.productService.newProduct());
+    this.store.dispatch(ProductActions.initializeCurrentProduct());
   }
 
   productSelected(product: Product): void {
-    this.productService.changeSelectedProduct(product);
+    this.store.dispatch(ProductActions.setCurrentProduct({ product }));
   }
 }
